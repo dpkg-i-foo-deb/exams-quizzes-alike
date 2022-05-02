0c7b52ccf348f686b1d95ecfdc2ba11981408694
Mateo Estrada Ramirez <mateoestradaramires2001@gmail.com>
2022-05-01 21:27:24 -0500

    Add initial database connection structure

---
+++ src/exams_quizzes_alike/lib/database/app_database.dart
@@ -0,0 +1,29 @@
+import 'package:postgres/postgres.dart';
+
+//Initial database connection, here's where you set your username, password and
+//database name
+class AppDatabase {
+  PostgreSQLConnection? connection;
+
+  PostgreSQLResult? result;
+
+  AppDatabase() {
+    connection = (connection == null || connection!.isClosed == true
+        ? PostgreSQLConnection(
+            // for external device like mobile phone use domain.com or
+            // your computer machine IP address (i.e,192.168.0.1,etc)
+            // when using AVD add this IP 10.0.2.2
+            'localhost',
+            5432,
+            'institucion',
+            username: 'institucion_admin',
+            password: 'institucion_admin',
+            timeoutInSeconds: 30,
+            queryTimeoutInSeconds: 30,
+            timeZone: 'UTC',
+            useSSL: false,
+            isUnixSocket: false,
+          )
+        : connection);
+  }
+}
