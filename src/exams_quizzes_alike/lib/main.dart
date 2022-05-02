d83ec63acc1f3c29112daf0fc5331de6a7575262
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-05-02 09:10:21 -0500

    Add initial login blank page

--- src/exams_quizzes_alike/lib/main.dart
+++ src/exams_quizzes_alike/lib/main.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
 
+import 'widgets/login_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(primaryColor: Colors.purple),
+    debugShowCheckedModeBanner: false,
+    home: const LoginPage(),
+  ));
+}
