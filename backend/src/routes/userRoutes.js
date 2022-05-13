c798611e87e4729789514935217af822ee56e42d
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-05-13 16:32:21 -0500

    Refactor

---
+++ backend/src/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const router = Router();
+
+//We add the methods we defined on the controllers
+const { getUsers, login } = require('../services/userService');
+
+router.get('/users', getUsers);
+router.post('/login',login);
+
+
+module.exports = router;
