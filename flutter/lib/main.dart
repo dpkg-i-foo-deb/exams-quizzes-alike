be7fcf49754e2fd237b3a10345c4f00384ced621
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-05-17 13:53:11 -0500

    Remove routes from main 🐛

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:exams_quizzes_alike/screens/login/login_page.dart';
 import 'package:flutter/material.dart';
-import 'package:exams_quizzes_alike/routes.dart';
 
 void main() {
   runApp(MaterialApp(
