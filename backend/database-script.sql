fd1c89fbe328e60e9f39e14b5a5ffe1aa5801c32
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-05-17 14:05:38 -0500

    Fix database script YES AGAIN 🐛

--- backend/database-script.sql
+++ backend/database-script.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS public.administrador
 
 CREATE TABLE IF NOT EXISTS public.categoria_examen
 (
-    codigo_categoria integer NOT NULL,
+    codigo_categoria integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     descripcion character varying COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT categoria_examen_pkey PRIMARY KEY (codigo_categoria)
 );
