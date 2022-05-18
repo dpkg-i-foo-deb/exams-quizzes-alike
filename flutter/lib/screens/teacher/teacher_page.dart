a6d4fed37e8937fb1caeb433c582ff7f7d8157c9
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-05-17 22:55:45 -0500

    Improve grid courses view ✨

--- flutter/lib/screens/teacher/teacher_page.dart
+++ flutter/lib/screens/teacher/teacher_page.dart
@@ -2,6 +2,7 @@ import 'package:exams_quizzes_alike/exceptions/course_exception.dart';
 import 'package:exams_quizzes_alike/models/course.dart';
 import 'package:exams_quizzes_alike/network/course_requests.dart';
 import 'package:exams_quizzes_alike/screens/teacher/components/course_item.dart';
+import 'package:exams_quizzes_alike/widgets/main_app_bar.dart';
 import 'package:flutter/material.dart';
 
 import '../../models/teacher.dart';
@@ -31,37 +32,42 @@ class _TeacherPageState extends State<TeacherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: const MainAppBar(),
         body: SingleChildScrollView(
-      child: FutureBuilder(
-          future: getCourses(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            return Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                padding: const EdgeInsets.all(25),
-                decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        colors: [
-                      Color.fromARGB(255, 191, 238, 183),
-                      Color.fromARGB(255, 215, 244, 210),
-                    ])),
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4,
-                  ),
-                  itemCount: courses.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return CourseItem(
-                            courses[index].courseCode!,
-                            courses[index].courseDescription!,
-                            courses[index].teacherLogin)
-                        .buildItem(context);
-                  },
-                ));
-          }),
-    ));
+          child: FutureBuilder(
+              future: getCourses(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                return Container(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    padding: const EdgeInsets.all(25),
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                          Color.fromARGB(255, 191, 238, 183),
+                          Color.fromARGB(255, 215, 244, 210),
+                        ])),
+                    child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4,
+                        childAspectRatio: 7 / 6,
+                        crossAxisSpacing: 20,
+                      ),
+                      itemCount: courses.length,
+                      padding: const EdgeInsets.all(1),
+                      itemBuilder: (BuildContext context, int index) {
+                        return CourseItem(
+                                courses[index].courseCode!,
+                                courses[index].courseDescription!,
+                                courses[index].teacherLogin)
+                            .buildItem(context);
+                      },
+                    ));
+              }),
+        ));
   }
 
   Future<List<Course>> getCourses() async {
