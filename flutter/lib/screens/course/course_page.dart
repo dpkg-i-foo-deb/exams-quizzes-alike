9db18410902216a6479e869315ed68424c148d5f
Laura Suárez <lauraa.suarezg@uqvirtual.edu.co>
2022-05-25 19:46:56 -0500

    course view modified and application with problems

--- flutter/lib/screens/course/course_page.dart
+++ flutter/lib/screens/course/course_page.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:exams_quizzes_alike/widgets/main_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CoursePage extends StatefulWidget {
   const CoursePage(
@@ -28,13 +31,18 @@ class _CoursePageState extends State<CoursePage> {
         child: Column(children: [
           Container(
               constraints: const BoxConstraints(
-                  maxHeight: 250, maxWidth: double.infinity),
+                  maxHeight: 100, maxWidth: double.infinity),
               decoration: BoxDecoration(
                   color: const Color.fromARGB(255, 237, 231, 246),
                   borderRadius: BorderRadius.circular(20)),
               child: Align(
-                child: Text(widget.courseName),
-                alignment: Alignment.center,
+                child: Text(widget.courseName,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontFamily: 'ZenLoop',
+                      fontSize: 70,
+                      color: Colors.black,
+                    )),
               )),
         ]),
       )),
