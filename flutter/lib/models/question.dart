bbd41a818fec74b0ed2ea705cbeccdc5b4572983
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-06-02 21:07:09 -0500

    Add question model 👷

---
+++ flutter/lib/models/question.dart
@@ -0,0 +1,37 @@
+class Question {
+  final String code;
+  final String isPublic;
+  final String isFather;
+  final String type;
+  final String weight;
+  final String literal;
+  final String subQuestionCode;
+  final String teacherLogin;
+  final String topicCode;
+
+  Question({
+    required this.code,
+    required this.isPublic,
+    required this.isFather,
+    required this.type,
+    required this.weight,
+    required this.literal,
+    required this.subQuestionCode,
+    required this.teacherLogin,
+    required this.topicCode,
+  });
+
+  factory Question.fromJson(Map<String, dynamic> json) {
+    return Question(
+      code: json['codigo_pregunta'],
+      isPublic: json['isPublic'],
+      isFather: json['isFather'],
+      type: json['tipo'],
+      weight: json['peso'],
+      literal: json['enunciado'],
+      subQuestionCode: json['codigo_subpregunta'],
+      teacherLogin: json['codigo_docente'],
+      topicCode: json['codigo_tema'],
+    );
+  }
+}
