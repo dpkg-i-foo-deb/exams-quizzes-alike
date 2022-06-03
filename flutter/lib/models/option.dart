7cab936aab71aa8f61a36f26f5057c3dd76771e5
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-06-02 22:55:17 -0500

    Add option model ➕

---
+++ flutter/lib/models/option.dart
@@ -0,0 +1,28 @@
+class Option {
+  final String code;
+  final String description;
+  final String correctAnswer;
+  final String missingWord;
+  final String sortOrder;
+  final String pair;
+
+  Option({
+    required this.code,
+    required this.description,
+    required this.correctAnswer,
+    required this.missingWord,
+    required this.pair,
+    required this.sortOrder,
+  });
+
+  factory Option.fromJson(Map<String, dynamic> json) {
+    return Option(
+      code: json['codigo_opcion'].toString(),
+      description: json['description'],
+      correctAnswer: json['respuesta_correcta'],
+      missingWord: json['palabra_faltante'],
+      sortOrder: json['orden'].toString(),
+      pair: json['pareja'],
+    );
+  }
+}
