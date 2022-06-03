12abf58eebaba098b1d1a5be27e5c0b294510a8b
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-06-02 19:15:40 -0500

    Add initial scheduled exam item 👷

---
+++ flutter/lib/screens/course/components/scheduled_exam_item.dart
@@ -0,0 +1,49 @@
+import 'package:exams_quizzes_alike/exceptions/exam_exception.dart';
+import 'package:exams_quizzes_alike/models/exam.dart';
+import 'package:exams_quizzes_alike/models/scheduled_exam.dart';
+import 'package:exams_quizzes_alike/network/exam_requests.dart';
+import 'package:exams_quizzes_alike/widgets/grid_item.dart';
+import 'package:flutter/material.dart';
+
+class ScheduledExamItem implements GridItem {
+  final ScheduledExam scheduledExam;
+  Exam? exam;
+  ScheduledExamItem(this.scheduledExam);
+
+  @override
+  Widget buildItem(BuildContext context) {
+    return Card(
+      color: const Color.fromARGB(255, 73, 89, 154),
+      child: InkWell(
+        splashColor: Colors.white,
+        onTap: () => Navigator.push(
+            context, MaterialPageRoute(builder: ((context) => null!))),
+        child: Center(
+            child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: FutureBuilder(
+                  future: getExam(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    return Text(
+                      scheduledExam.date,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Colors.white),
+                      textAlign: TextAlign.center,
+                    );
+                  },
+                ))),
+      ),
+    );
+  }
+
+  Future<void> getExam() async {
+    try {
+      exam = await ExamRequests().getExam(int.parse(scheduledExam.examCode));
+    } on ExamException {
+      //TODO something when we cannot get the exam
+      rethrow;
+    }
+  }
+}
