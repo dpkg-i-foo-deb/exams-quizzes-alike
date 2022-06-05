45bcdf8497af547b7dfd4f69713ba7673ce5c39a
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-06-05 17:12:16 -0500

    Add more backend services ➕

--- backend/src/routes/courseRoutes.js
+++ backend/src/routes/courseRoutes.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getCoursesByTeacher } = require('../services/courseService')
+const { getCoursesByTeacher, getCoursesByStudent, getCourse } = require('../services/courseService')
 
 router.post('/courses-by-teacher',getCoursesByTeacher);
+router.post('/courses-by-student',getCoursesByStudent);
+router.post('/get-course',getCourse);
 
 module.exports = router;
