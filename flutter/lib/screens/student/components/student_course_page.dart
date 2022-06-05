e868f3fcd60edf494a8bb88f03262324db2b090c
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-06-05 18:16:56 -0500

    Improve student course page ✨

--- flutter/lib/screens/student/components/student_course_page.dart
+++ flutter/lib/screens/student/components/student_course_page.dart
@@ -1,4 +1,6 @@
 import 'package:exams_quizzes_alike/models/course.dart';
+import 'package:exams_quizzes_alike/screens/student/components/student_course_page_body.dart';
+import 'package:exams_quizzes_alike/widgets/main_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class StudentCoursePage extends StatefulWidget {
@@ -13,6 +15,10 @@ class StudentCoursePage extends StatefulWidget {
 class _StudentCoursePageState extends State<StudentCoursePage> {
   @override
   Widget build(BuildContext context) {
-    return Text('oli');
+    return Scaffold(
+        appBar: MainAppBar(),
+        body: SingleChildScrollView(
+          child: StudentCoursePageBody(course: widget.course),
+        ));
   }
 }
---
+++ flutter/lib/screens/student/components/student_course_page_body.dart
@@ -0,0 +1,41 @@
+import 'package:exams_quizzes_alike/models/course.dart';
+import 'package:flutter/material.dart';
+
+class StudentCoursePageBody extends StatefulWidget {
+  const StudentCoursePageBody({Key? key, required this.course})
+      : super(key: key);
+
+  final Course course;
+
+  @override
+  State<StudentCoursePageBody> createState() => _StudentCoursePageBodyState();
+}
+
+class _StudentCoursePageBodyState extends State<StudentCoursePageBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(children: [
+          Container(
+            constraints:
+                const BoxConstraints(maxHeight: 200, maxWidth: double.infinity),
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 237, 231, 246),
+                borderRadius: BorderRadius.circular(20)),
+            child: Align(
+              child: Text(widget.course.courseDescription!,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontFamily: 'ZenLoop',
+                    fontSize: 70,
+                    color: Colors.black,
+                  )),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          )
+        ]));
+  }
+}
