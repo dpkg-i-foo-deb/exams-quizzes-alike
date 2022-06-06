9f334ad4bcc16ccbd5f74c498d3226401bac7cab
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-06-05 20:44:29 -0500

    fix scheduled exam service AGAIN 🐛

--- backend/src/services/scheduledExamService.js
+++ backend/src/services/scheduledExamService.js
@@ -14,10 +14,10 @@ const getAvailableExams = async (req,res) =>
 
     course_code = parseInt(course_code);
 
-    const response = await connectionPool.query(`SELECT  e.codigo_examen, ce.codigo_curso FROM curso_examen ce 
+    const response = await connectionPool.query(`	SELECT  e.codigo_examen, ce.codigo_curso, to_char((ce.fecha_presentacion + e.tiempo_limite),'YYYY-MM-DD HH:MM:SS') fecha_maxima FROM curso_examen ce 
     JOIN (select e.codigo_examen codigo_examen ,e.tiempo_limite tiempo_limite  from examen e) e ON e.codigo_examen = ce.codigo_examen
                          WHERE NOW() > ce.fecha_presentacion and NOW() < (ce.fecha_presentacion + e.tiempo_limite)at TIME zone 'America/Bogota' AND ce.codigo_curso = $1
-    GROUP BY e.codigo_examen ,ce.codigo_curso`,[course_code]);
+    GROUP BY e.codigo_examen ,ce.codigo_curso, ce.fecha_presentacion, e.tiempo_limite`,[course_code]);
 
     if(response.rows.length<1)
     {
