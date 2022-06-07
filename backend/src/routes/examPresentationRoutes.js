f5a667da89987b64eb9c0c79aa5d64c4fe48d814
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-06-06 23:12:23 -0500

    Improve backend 🎉

--- backend/src/routes/examPresentationRoutes.js
+++ backend/src/routes/examPresentationRoutes.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 
-const {getPresentation, createPresentation, setPresentationQuestion, getPresentationQuestion} = require('../services/examPresentationService');
+const {getPresentation, createPresentation, setPresentationQuestion, getPresentationQuestion, getExamQuestionCode} = require('../services/examPresentationService');
 
 router.post('/get-presentation',getPresentation);
 router.post('/create-presentation',createPresentation);
 router.post('/set-presentation-question',setPresentationQuestion);
 router.post('/get-presentation-question',getPresentationQuestion);
+router.post('/get-exam-question-code',getExamQuestionCode);
 
 module.exports = router;
